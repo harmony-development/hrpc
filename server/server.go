3169eea718a03fd2d589282f5b9dba63ef6576e1
Jan Blackquill <uhhadd@gmail.com>
2021-01-29 18:05:37 -0500

    Add ChainHandlerTransformers for multiple middleware handlers

--- server/server.go
+++ server/server.go
@@ -22,6 +22,32 @@ type HRPCServer struct {
 type Handler func(c context.Context, req protoreflect.ProtoMessage, headers http.Header) (protoreflect.ProtoMessage, error)
 type HandlerTransformer func(meth *descriptorpb.DescriptorProto, d *descriptorpb.FileDescriptorProto, h Handler) Handler
 
+func ChainHandlerTransformers(funs ...HandlerTransformer) HandlerTransformer {
+	switch len(funs) {
+	case 0:
+		return nil
+	case 1:
+		return funs[0]
+	default:
+		// {a, b, c}[0] -> a
+		fn := funs[0]
+		// {a, b, c}[1:] -> {b, c}
+		other := funs[1:]
+		// b, c
+		// fn = b(a(handler))
+		// c
+		// fn = c(b(a(handler)))
+		// or
+		// handler |> a |> b |> c
+		for _, fun := range other {
+			fn = func(meth *descriptorpb.DescriptorProto, d *descriptorpb.FileDescriptorProto, h Handler) Handler {
+				return fn(meth, d, fun(meth, d, h))
+			}
+		}
+		return fn
+	}
+}
+
 func NewHRPCServer(items ...HRPCServiceHandler) *HRPCServer {
 	herpes := &HRPCServer{
 		handlers: items,
