d6d317daf517b1245f90f27dd48b9998d953d215
Jan Blackquill <uhhadd@gmail.com>
2021-01-31 21:48:28 -0500

    dart stub

---
+++ gendartprotos.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+mkdir -p "gendart"
+
+for dir in $(find "protocol" -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq); do
+    echo "Generating files in ${dir}..."
+    find "${dir}" -name '*.proto'
+
+    protoc \
+    --proto_path=protocol \
+    --dart_out=./gendart \
+    --hrpc_out=./gendart \
+    --hrpc_opt=hrpc-client-dart \
+    $(find "${dir}" -name '*.proto')
+done
