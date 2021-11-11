2e38b014dbc92215be69956459f3602fae3b999b
Bluskript <bluskript@gmail.com>
2021-11-11 01:35:36 -0500

    fix chat demo

--- examples/chat/chat-client/gen/chat.client.ts
+++ examples/chat/chat-client/gen/chat.client.ts
@@ -3,44 +3,56 @@
 // tslint:disable
 import type { RpcTransport } from "@protobuf-ts/runtime-rpc";
 import type { ServiceInfo } from "@protobuf-ts/runtime-rpc";
-import { ChatService } from "./chat";
+import { Chat } from "./chat";
 import type { ServerStreamingCall } from "@protobuf-ts/runtime-rpc";
 import { stackIntercept } from "@protobuf-ts/runtime-rpc";
-import type { Empty } from "./google/protobuf/empty";
+import type { Empty } from "./chat";
 import type { Message } from "./chat";
 import type { UnaryCall } from "@protobuf-ts/runtime-rpc";
 import type { RpcOptions } from "@protobuf-ts/runtime-rpc";
 /**
- * @generated from protobuf service chat.ChatService
+ * Chat service.
+ *
+ * @generated from protobuf service chat.Chat
  */
-export interface IChatServiceClient {
+export interface IChatClient {
     /**
-     * @generated from protobuf rpc: SendMessage(chat.Message) returns (google.protobuf.Empty);
+     * Endpoint to send a chat message.
+     *
+     * @generated from protobuf rpc: SendMessage(chat.Message) returns (chat.Empty);
      */
     sendMessage(input: Message, options?: RpcOptions): UnaryCall<Message, Empty>;
     /**
-     * @generated from protobuf rpc: StreamMessages(google.protobuf.Empty) returns (stream chat.Message);
+     * Endpoint to stream chat messages.
+     *
+     * @generated from protobuf rpc: StreamMessages(chat.Empty) returns (stream chat.Message);
      */
     streamMessages(input: Empty, options?: RpcOptions): ServerStreamingCall<Empty, Message>;
 }
 /**
- * @generated from protobuf service chat.ChatService
+ * Chat service.
+ *
+ * @generated from protobuf service chat.Chat
  */
-export class ChatServiceClient implements IChatServiceClient, ServiceInfo {
-    typeName = ChatService.typeName;
-    methods = ChatService.methods;
-    options = ChatService.options;
+export class ChatClient implements IChatClient, ServiceInfo {
+    typeName = Chat.typeName;
+    methods = Chat.methods;
+    options = Chat.options;
     constructor(private readonly _transport: RpcTransport) {
     }
     /**
-     * @generated from protobuf rpc: SendMessage(chat.Message) returns (google.protobuf.Empty);
+     * Endpoint to send a chat message.
+     *
+     * @generated from protobuf rpc: SendMessage(chat.Message) returns (chat.Empty);
      */
     sendMessage(input: Message, options?: RpcOptions): UnaryCall<Message, Empty> {
         const method = this.methods[0], opt = this._transport.mergeOptions(options);
         return stackIntercept<Message, Empty>("unary", this._transport, method, opt, input);
     }
     /**
-     * @generated from protobuf rpc: StreamMessages(google.protobuf.Empty) returns (stream chat.Message);
+     * Endpoint to stream chat messages.
+     *
+     * @generated from protobuf rpc: StreamMessages(chat.Empty) returns (stream chat.Message);
      */
     streamMessages(input: Empty, options?: RpcOptions): ServerStreamingCall<Empty, Message> {
         const method = this.methods[1], opt = this._transport.mergeOptions(options);
--- examples/chat/chat-client/src/App.vue
+++ examples/chat/chat-client/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { ChatServiceClient } from "../gen/chat.client";
+import { ChatClient } from "../gen/chat.client";
 import { HrpcTransport } from "@harmony-dev/transport-hrpc";
 import { onMounted, reactive, ref } from "vue";
-const client = new ChatServiceClient(
+const client = new ChatClient(
   new HrpcTransport({
-    baseUrl: "http://localhost:6969",
+    baseUrl: "http://127.0.0.1:2289",
+    insecure: true
   })
 );
 
@@ -28,8 +29,8 @@ const send = (ev: KeyboardEvent) => {
 
 <template>
   <div class="h-100vh w-100vw bg-surface-900 flex flex-col justify-center p-3">
-    <div class="flex-1 p-3">
-      <p v-for="m in msgs" :key="m">{{ m }}</p>
+    <div class="flex-1 p-3 flex flex-col gap-2 overflow-auto">
+      <p class="p-3 max-w-30ch rounded-md bg-surface-800" v-for="m in msgs" :key="m">{{ m }}</p>
     </div>
     <input
       class="
@@ -38,6 +39,7 @@ const send = (ev: KeyboardEvent) => {
         rounded-md
         focus:outline-none focus:ring-3
         ring-secondary-400
+	mt-2
       "
       v-model="content"
       @keydown="send"
